57079653c1fd781bad471ee502d784c58a167bbc
EkkiRu <63056889+EkkiRu@users.noreply.github.com>
2020-11-20 15:03:18 +0600

    Homework
    Homework

---
+++ ClientAppSocket/ClientAppSocket/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Shell;
+
+namespace ClientAppSocket
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Send(object sender, RoutedEventArgs e)
+        {
+            // аддресс интранет сети, адрес локальный и адрес localhost
+            // 1 тип вдреса узнаём через командную строку и команду ipconfig
+
+            var ipAddress = IPAddress.Parse("127.0.0.1");
+
+            //создыем сокет для ip сети с транспортным протоколом TCP, работающий через потоки (постоянные соединения)
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                await socket.ConnectAsync(new IPEndPoint(ipAddress, 443));
+                var text = textBox.Text;
+
+                var data = Encoding.UTF8.GetBytes(text);
+
+                await socket.SendAsync(data, SocketFlags.None);
+                socket.Close();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+    }
+}
---
+++ ServerAppSocket/ServerAppSocket/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerAppSocket
+{
+    class Program
+    {
+        static async void Main(string[] args)
+        {
+            // аддресс интранет сети, адрес локальный и адрес localhost
+            // 1 тип вдреса узнаём через командную строку и команду ipconfig
+
+            var ipAddress = IPAddress.Parse("127.0.0.1");
+
+            //создыем сокет для ip сети с транспортным протоколом TCP, работающий через потоки (постоянные соединения)
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                //привязываем наш соккет к конечной точке для будущего прослушивания 
+                socket.Bind(new IPEndPoint(ipAddress, 62227));
+                //начинаем прослушивать конечную  точку на входяшщие соединение при этом может держать в ожидании 10 следующий соединений
+                socket.Listen(10);
+
+                Console.WriteLine("Сервер запущен...");
+                //бесконечно принимаем входящие соединения и считываем из них данные
+                while (true)
+                {
+                    Console.WriteLine("Ждем входящих соединений...");
+                    var incomeSocket = await socket.AcceptAsync();
+                    //в зависимости что мы хотим сделать будет меняться 1...<
+                    var buffer = new byte[256];
+                    //работает как read у Stream
+                    do
+                    {
+                        SocketAsyncEventArgs socketAsyncEventArgs = new SocketAsyncEventArgs();
+                        socketAsyncEventArgs.SetBuffer(buffer);
+                        incomeSocket.ReceiveAsync(socketAsyncEventArgs);
+                        socketAsyncEventArgs.Dispose();
+
+                        Console.WriteLine($"Новое сообщение: {Encoding.UTF8.GetString(buffer)}");
+                    }
+                    while (incomeSocket.Available > 0);
+                    incomeSocket.Shutdown(SocketShutdown.Both);
+                    incomeSocket.Close();
+                    Console.WriteLine("Закрытие соединения...");
+                    //>...2
+                }
+
+                //socket.Close();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+    }
+}
